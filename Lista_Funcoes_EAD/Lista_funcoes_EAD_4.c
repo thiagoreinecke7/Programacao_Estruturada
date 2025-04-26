2ebc58c64e41e9a212a7d3c1bf1a65e168d75f55
Thiago <thiagoreinecke7@gmail.com>
2025-04-26 13:59:52 -0300

    Finalização ex4 Lista_funcoes_EAD

--- Lista_Funcoes_EAD/Lista_funcoes_EAD_4.c
+++ Lista_Funcoes_EAD/Lista_funcoes_EAD_4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <time.h>
 
 int maior(int a, int b){
 
@@ -12,17 +11,14 @@ int maior(int a, int b){
 }
 
 int main(){
-    int vetor[10], ordenador = 0, auxiliar;
-    srand(time(NULL));
-
+    int vetor[10], ordenador = 0, auxiliar, num_maior;
 
     for(int i = 0; i < 10; i++){
-        /*printf("Digite o %dº número: ", i + 1);
-        scanf("%d", &vetor[i]);*/
-        vetor[i] = rand()%15 + 0;
+        printf("Digite o %dº número: ", i + 1);
+        scanf("%d", &vetor[i]);
     }
 
-    printf("Vetor escrito foi: [ ");
+    printf("\nVetor escrito foi: [ ");
     for(int i = 0; i < 10; i++){
         printf("%d ", vetor[i]);
     }
@@ -39,10 +35,38 @@ int main(){
 
     if(ordenador == 1){
         for(int i = 0; i < 10; i++){
+            for(int n = 0; n < 9; n++){
+                num_maior = maior(vetor[n], vetor[n +1]);
 
+                if(num_maior == vetor[n]){
+                    auxiliar = vetor[n + 1];
+                    vetor[n + 1] = num_maior;
+                    vetor[n] = auxiliar;
+                }
+            }
         }
+        printf("\nVetor na forma crescente: [ ");
+        for(int i = 0; i < 10; i++){
+            printf("%d ", vetor[i]);
+        }
+        printf("]\n\n");
     }else{
+        for(int i = 10; i > 0; i--){
+            for(int n = 9; n > 0; n--){
+                num_maior = maior(vetor[n], vetor[n - 1]);
 
+                if(num_maior == vetor[n]){
+                    auxiliar = vetor[n - 1];
+                    vetor[n - 1] = num_maior;
+                    vetor[n] = auxiliar;
+                }
+            }
+        }
+        printf("\nVetor na forma decrescente: [ ");
+        for(int i = 0; i < 10; i++){
+            printf("%d ", vetor[i]);
+        }
+        printf("]\n\n");
     }
 
     return 0;
